175a664465b078531dfea4013916ba13e7d573c5
Jeremy Gao <145064583+WenSimEHRP@users.noreply.github.com>
2024-03-11 23:12:05 -0700

    Add: example of complicated station

--- build.sh
+++ build.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-while true
-do
-    python3 src/stations/generated_stations/templates/generate_templates.py
-    python3 get_date.py
-    python3 generate_stations.py
-    python3 combine.py
-    gcc -E -x c -o wsjps.nml wsjps.pnml
-    gcc -E -x c -P -o lang/english.lng lang/anglische.lng.template
-    nmlc wsjps.nml
-
-    # move to newgrf directory
-    # cp wsjps.grf /D/Data/Documents/OpenTTD/newgrf
-    read -p "Press any key to continue . . . "
-done
+python3 src/stations/generated_stations/templates/generate_templates.py
+python3 get_date.py
+python3 generate_stations.py
+python3 combine.py
+gcc -E -x c -o wsjps.nml wsjps.pnml
+gcc -E -x c -P -o lang/english.lng lang/anglische.lng.template
+nmlc wsjps.nml
