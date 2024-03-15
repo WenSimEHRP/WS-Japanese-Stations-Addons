1ee30dded196787d53e4fe974af96ccc08443d67
Jeremy Gao <145064583+WenSimEHRP@users.noreply.github.com>
2024-03-14 23:00:20 -0700

    Add: Multiple subjects for teststation add full support of date-specific graphics add full support of edge platform graphics add single platforms

--- build.bat
+++ build.bat
@@ -6,7 +6,7 @@ py generate_stations.py
 py combine.py
 gcc -E -x c -o wsjps.nml wsjps.pnml
 gcc -E -x c -P -o lang\english.lng lang\anglische.lng.template
-nmlc wsjps.nml
+nmlc -c wsjps.nml
 
 :: move to newgrf directory
 copy wsjps.grf D:\Data\Documents\OpenTTD\newgrf
